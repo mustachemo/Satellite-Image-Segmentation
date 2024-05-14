089c84a5ec8a9890958586b9e397979e57275854
Mohamed <mhasan1080@gmail.com>
2024-05-13 19:34:14 -0700

    progress towards tf unet

--- model/is_model.py
+++ model/is_model.py
@@ -1,120 +1,53 @@
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
-
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
-
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
-
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
 
 
-""" Parts of the U-Net model """
+def downsample_block(input_tensor, num_filters):
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    return x, layers.MaxPooling2D(pool_size=(2, 2))(x)
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+def double_conv_block(input_tensor, num_filters):
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    return layers.Activation('relu')(x)
 
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
+def upsample_block(input_tensor, skip_tensor, num_filters):
+    x = layers.Conv2DTranspose(num_filters, (2, 2), strides=(2, 2), padding='same')(input_tensor)
+    x = layers.concatenate([x, skip_tensor])
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(num_filters, (3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    return layers.Activation('relu')(x)
 
 
-class Up(nn.Module):
-    """Upscaling then double conv"""
+def build_unet_model():
+    # inputs
+   inputs = layers.Input(shape=(128,128,3))
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
+   f1, p1 = downsample_block(inputs, 64)
+   f2, p2 = downsample_block(p1, 128)
+   f3, p3 = downsample_block(p2, 256)
+   f4, p4 = downsample_block(p3, 512)
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+   bottleneck = double_conv_block(p4, 1024)
 
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
+   u6 = upsample_block(bottleneck, f4, 512)
+   u7 = upsample_block(u6, f3, 256)
+   u8 = upsample_block(u7, f2, 128)
+   u9 = upsample_block(u8, f1, 64)
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
+   outputs = layers.Conv2D(3, 1, padding="same", activation = "softmax")(u9)
 
+   unet_model = Model(inputs, outputs, name="U-Net")
+   return unet_model
 
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x):
-        return self.conv(x)
