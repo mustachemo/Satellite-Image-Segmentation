fixed scaling issue of masks

uncertainity_quantification/MC_dropout.py
@@ -2,6 +2,7 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from utils.logger_prep import get_logger
+from configs import X_DIMENSION, Y_DIMENSION
 
 logger = get_logger(__name__)
 
@@ -55,6 +56,7 @@ def mc_dropout_predictions(model, input_image, num_samples=50):
     input_image = np.expand_dims(input_image, axis=0)
     predictions = [model(input_image, training=True) for _ in tqdm(range(num_samples), desc='MC Dropout Inference for a single test image')]
     predictions = np.array(predictions)
+
     return predictions
 
 
@@ -88,6 +90,46 @@ def visualize_mean_std(test_image, test_mask, mean_prediction, std_deviation):
     
     plt.show()
 
+def visualize_mean_std_grid(test_images, test_masks, predictions, rows=4, cols=5):
+    """
+    Visualize the test images, masks, mean predictions, and uncertainty across multiple samples.
+    
+    Parameters:
+        test_images (numpy.array): Array of test images.
+        test_masks (numpy.array): Array of corresponding masks.
+        predictions (list): List of predictions arrays, each from a different MC sample.
+        num_samples (int): Number of MC samples to visualize.
+        rows (int): Number of rows in the grid.
+        cols (int): Number of columns in the grid.
+    """
+    fig, axes = plt.subplots(rows, cols, figsize=(3 * cols + 10, 3 * rows + 10))
+    
+    for i in range(cols):
+        for j in range(rows):
+            ax = axes[j, i]
+            mean_prediction = np.mean(predictions[i], axis=0)
+            std_deviation = np.std(predictions[i], axis=0)
+            
+            if j == 0:  # First row - Test Image
+                im = ax.imshow(test_images[i])
+                ax.set_title(f'Test Image[{i+1}]')
+            elif j == 1:  # Second row - Test Mask
+                im = ax.imshow(test_masks[i][:, :, 0], cmap='gray')
+                ax.set_title(f'Test Mask[{i+1}]')
+            elif j == 2:  # Third row - Mean Prediction
+                im = ax.imshow(mean_prediction[0, :, :, 0], cmap='gray')
+                ax.set_title(f'Mean Prediction[{i+1}]')
+            elif j == 3:  # Fourth row - Prediction Uncertainty
+                im = ax.imshow(std_deviation[0, :, :, 0], cmap='gray')
+                ax.set_title(f'Uncertainty[{i+1}]')
+            
+            ax.axis('off')
+            if i == cols - 1:
+                fig.colorbar(im, ax=ax)
+
+    plt.tight_layout(pad=3.0, h_pad=3.0)
+    plt.show()
+
 
 # Confidence Interval Visualizations
 def visualize_confidence_intervals(test_image, mean_prediction, std_deviation, confidence_level=0.95):
@@ -102,13 +144,13 @@ def visualize_confidence_intervals(test_image, mean_prediction, std_deviation, c
     # Display the lower bound of the confidence interval
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    plt.imshow(lower_bound[0, :, :, 0], cmap='gray')
+    plt.imshow(lower_bound[0, :, :, 0], cmap='gray', vmin=0, vmax=1)
     plt.colorbar()
     plt.title(f'{confidence_level * 100}% Confidence Lower Bound')
     
     # Display the upper bound of the confidence interval
     plt.subplot(1, 2, 2)
-    plt.imshow(upper_bound[0, :, :, 0], cmap='gray')
+    plt.imshow(upper_bound[0, :, :, 0], cmap='gray', vmin=0, vmax=1)
     plt.colorbar()
     plt.title(f'{confidence_level * 100}% Confidence Upper Bound')
     

utils/data_loader_unet.py
@@ -25,8 +25,8 @@ def load_and_process_files(image_dir, mask_dir, prefix='train'):
 
 
         # Convert to array and normalize
-        image = img_to_array(image) / 255.0
-        mask = img_to_array(mask) / 255.0
+        image = img_to_array(image, dtype=np.float32) / np.max(image)
+        mask = img_to_array(mask, dtype=np.float32) / np.max(mask)
 
         # Convert to tensors
         image = tf.convert_to_tensor(image, dtype=tf.float32)