updated function to fix masking
after switching to binary masking, we did not scale the mask values of the differnet colors, resulting in greys masks

main.py
@@ -18,6 +18,8 @@ from uncertainty_quantification.MC_dropout import (
 
 if __name__ == '__main__':
 
+    # visualize('421')
+
     logger = get_logger(__name__)
 
     # Load the model and test data
@@ -39,18 +41,18 @@ if __name__ == '__main__':
     # Uncertainty quantification using MC dropout #
     #######################################################################
     # Perform MC dropout inference on a single test image
-    # mc_predictions = mc_dropout_predictions(model, test_images[0], num_samples=NUM_SAMPLES_MC_DROPOUT_PREDICTION)
-    # mean_prediction = np.mean(mc_predictions, axis=0)
-    # std_deviation = np.std(mc_predictions, axis=0)
-    # visualize_mean_std(test_images[0], test_masks[0], mean_prediction, std_deviation)
-    # visualize_confidence_intervals(test_images[0], mean_prediction, std_deviation, confidence_level=0.95)
-    # plot_correlation_analysis(mean_prediction, std_deviation)
-    # get_uncertainty_avgs(mean_prediction, std_deviation)
+    mc_predictions = mc_dropout_predictions(model, test_images[0], num_samples=NUM_SAMPLES_MC_DROPOUT_PREDICTION)
+    mean_prediction = np.mean(mc_predictions, axis=0)
+    std_deviation = np.std(mc_predictions, axis=0)
+    visualize_mean_std(test_images[0], test_masks[0], mean_prediction, std_deviation)
+    visualize_confidence_intervals(test_images[0], mean_prediction, std_deviation, confidence_level=0.95)
+    plot_correlation_analysis(mean_prediction, std_deviation)
+    get_uncertainty_avgs(mean_prediction, std_deviation)
 
     # Perform MC dropout inference on all test images
-    # mean_of_means, mean_of_stds = run_mc_dropout_on_all_images(model, test_images, num_samples=NUM_SAMPLES_MC_DROPOUT_PREDICTION)
-    # plot_correlation_analysis(mean_of_means, mean_of_stds)
-    # get_uncertainty_avgs(mean_of_means, mean_of_stds)
+    mean_of_means, mean_of_stds = run_mc_dropout_on_all_images(model, test_images, num_samples=NUM_SAMPLES_MC_DROPOUT_PREDICTION)
+    plot_correlation_analysis(mean_of_means, mean_of_stds)
+    get_uncertainty_avgs(mean_of_means, mean_of_stds)
 
     # Visualize the mean and standard deviation for GRID_ITERATIONS number of test images
     predictions = np.array([mc_dropout_predictions(model, test_image, num_samples=NUM_SAMPLES_MC_DROPOUT_PREDICTION) for test_image in test_images[:GRID_ITERATIONS]])